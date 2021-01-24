1be9f742034dbf055a503547eeb57d2749985272
mgr <mgr@voxteneo.com>
2021-01-24 17:21:20 +0700

    Add project files.

---
+++ HelloGithubActions/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloGithubActions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
